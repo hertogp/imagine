88963e1b52170214c9cbfac7982040ff7c085d08
hertogp <git.hertogp@github.com>
2017-04-30 00:55:00 +0200

    updating documentation

--- update_docu.sh
+++ update_docu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
+cd ~/dev/imagine
 echo "updating README .."
-pandoc --filter imagine README.md -t markdown -o README
+pandoc --filter ./imagine.py README.md -t markdown -o README
 echo "Done!"
+cd ~/dev/imagine/examples
+echo
+echo "updating sample.pdf"j
+pandoc --filter ../imagine.py sample.md -o sample.pdf
+echo "Done!"
+cd ~/dev/imagine
+
 
