d35f9f25d4ebe59202e1e602e12efe0c82287c42
hertogp <git.hertogp@github.com>
2017-05-02 21:21:57 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,19 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'pandoc-imagine',
-    packages = ['pandoc-imagine'],
     version = '0.1.0',
+
+    # packages = ['pandoc-imagine'],
+
     description = 'A filter to process codeblocks into images or ascii art',
     long_description = long_description,
+
     author = 'hertogp',
     author_email = 'git.hertogp@gmail.com',
+
     url = 'https://github.com/hertogp/imagine',
-    download_url = 'https://github.com/hertogp/imagine/archive/0.1.tar.gz',
+    # download_url = 'https://github.com/hertogp/imagine/archive/0.1.0.tar.gz',
+
     keywords = ['pandoc', 'filter', 'codeblock', 'image', 'ascii-art'],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -26,5 +31,31 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Text Processing :: Filters'
-  ],
+        'Natural Language :: English',
+    ],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    py_modules=["pandoc_imagine"],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'pandoc-imagine = pandoc_imagine:main',
+        ],
+    },
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pandocfilters>=1.4'],
+
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
 )
