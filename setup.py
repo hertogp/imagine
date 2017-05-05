72fda0899bcba23c02315265b974c902fe71583a
hertogp <git.hertogp@github.com>
2017-05-05 17:27:04 +0200

    added py3.5

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Text Processing :: Filters',
         'Natural Language :: English',
     ],
