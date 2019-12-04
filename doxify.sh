cba195ed24cb311b5cd52bcbd52cd4c44045e926
hertogp <git.hertogp@gmail.com>
2019-12-04 22:37:04 +0100

    add notes in doxify.sh

--- doxify.sh
+++ doxify.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 cd ~/dev/imagine
 
-echo "creating README.md"
-# pay_the_pypir replaces the image-links to point to github rawcontent
-# since (test.)pypi.org won't host images.
+echo "
+Creating README.md
+ + runs imagine filter on _readme.md -> README.md
+ + uses Github-Flavored Markdown (-t gfm) as target format
+ + uses pay_the_pypir to fix image links so they're absolute links pointing
+   to github raw content, since pypi.org won't host the images.
 
-# pandoc --filter ./pandoc_imagine.py _readme.md -t markdown -o README.md
-# PYPI supports git-flavored markdown
+"
+
 pandoc --filter ./pandoc_imagine.py _readme.md -t gfm | awk -f pay_the_pypir > README.md
 
-#echo "creating README.rst"
-#pandoc --filter ./pandoc_imagine.py _readme.md -t rst | awk -f pay_the_pypir > README.rst
+echo "\
+Creating source distribution in ./dist
+"
 
-# echo "updating sample.pdf"
-# cd examples
-# pandoc --filter ../pandoc_imagine.py sample.md -o sample.pdf
-# cd ..
-
-echo ""
-echo "Creating source distribution in ./dist"
 python3 setup.py sdist
 
 echo "\
 
-TEST upload +-> twine upload --repository-url https://test.pypi.org/legacy/ ./dist/*
+TEST:
+ +--> twine upload --repository-url https://test.pypi.org/legacy/ ./dist/*
 
-PYPI
- - BEFORE uploading to pypi.org:
- + git add *
- + git commit 'release message'
- + git tag   # to see current tags
- + git tag 0.1.x -m 'version 0.1.x'
- + git push --tags origin master
- + python3 setup.py sdat
- |
- +-> twine upload --repository-url https://pypi.org/legacy/ ./dist*
+PYPI:
+ + BEFORE uploading to pypi.org
+ +  nvim pandoc_imagine.py --> remove -rcy from __version__ = '0.1.x-rcy'
+ +
+ + UPDATE GIT w/ tags
+ +  git add *
+ +  git commit 'release message'
+ +  git tag   # to see current tags
+ +  git tag 0.1.x -m 'version 0.1.x'
+ +  git push --tags origin master
+ +
+ + NEW SDIST
+ +  python3 setup.py sdat
+ +
+ + UPLOAD
+ +--> twine upload --repository-url https://pypi.org/legacy/ ./dist*
+
+"
 
